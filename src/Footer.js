3815a7d3533bc3d75c6122767138e710a8bbd70e
Snehal Sonawane <sonawane@Snehals-MacBook-Pro.local>
2024-07-16 15:11:53 +0530

    Updated footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,26 +1,24 @@
 import React, { useEffect } from 'react';
-function Footer() {
+const loadFooter = async () => {
+    if (!window.footerInitialized) {
+        window.footerInitialized = true;
+        const module = await import('https://feds--milo--adobecom.hlx.page/libs/navigation/footer.js');
+        module.default({ contentRoot: '/federal/home', miloLibs: 'https://feds--milo--adobecom.hlx.page' });
+    }
+  };
+  
+  const Footer = () => {
     useEffect(() => {
-        const initializeFooter = async () => {
-            if (!window.footerInitialized) {
-              window.footerInitialized = true;
-              window.fedsGlobalConfig = {
-                contentRoot: '/federal/home',
-                miloLibs: 'https://feds--milo--adobecom.hlx.page'
-              };
-              const { default: loadFooter } = await import('https://feds--milo--adobecom.hlx.page/libs/navigation/footer.js');
-              loadFooter(window.fedsGlobalConfig);
-            }
-          };
-          initializeFooter();
-        }, []);
+      loadFooter();
+    }, []);
+
     return (
       <>
-        <header className="global-navigation"></header>
+        
         <main style={{ padding: '60px' }}>
           <section>Consumer Project Content</section>
         </main>
-        <footer className="global-footer"></footer>
+       
       </>
     );
   }
